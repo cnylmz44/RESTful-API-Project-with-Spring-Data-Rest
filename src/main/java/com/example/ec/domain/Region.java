475516af336a8e4767a7e949581adcb07ffa7da1
can <canylmz3444@gmail.com>
2022-11-16 12:06:06 +0300

    spring data rest project

---
+++ src/main/java/com/example/ec/domain/Region.java
@@ -0,0 +1,25 @@
+package com.example.ec.domain;
+
+/**
+ * Enumeration of the region of California.
+ *
+ * Created by Can YILMAZ
+ */
+public enum Region {
+	Central_Coast("Central Coast"), Southern_California("Southern California"),
+	Northern_California("Northern California"), Varies("Varies");
+
+	private String label;
+
+	private Region(String label) {
+		this.label = label;
+	}
+
+	public static Region findByLabel(String byLabel) {
+		for (Region region : Region.values()) {
+			if (region.label.equalsIgnoreCase(byLabel))
+				return region;
+		}
+		return null;
+	}
+}
